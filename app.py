8b9eba6571a8149f1496ccf8b048715bad62ebf0
Paco Contreras <pacocontrerasdct@gmail.com>
2020-06-02 20:32:42 +0100

    site version 1 with content mocked

--- app.py
+++ app.py
@@ -1,40 +1,37 @@
 from flask import Flask, render_template, redirect, url_for, request
-from meta_tags_dict import pageTitleDict
+from meta_tags_dict import *
 
 app=Flask(__name__)
 
-
-@app.route("/layout")
-def layout():
-  return render_template("layout.html")
-
 @app.route("/")
 def index():
-  return render_template("layout.html",
-                header='header.html',
-                menu='menu.html',
-                footer='footer.html',
-                page="index.html",
-                titleText=pageTitleDict["index"])
+  return render_template("index.html",
+                titleText=pageTitleDict["index"],
+                headerText=headerDict["index"])
 
 @app.route("/historical")
 def historical():
-  return render_template("layout.html",
-                header='header.html',
-                menu='menu.html',
-                footer='footer.html',
-                page="historical.html",
-                titleText=pageTitleDict["historical"])
-
+  return render_template("historical.html",
+                titleText=pageTitleDict["historical"],
+                headerText=headerDict["historical"])
 
 @app.route("/about")
 def about():
-  return render_template("layout.html",
-                header='header.html',
-                menu='menu.html',
-                footer='footer.html',
-                page="about.html",
-                titleText=pageTitleDict["about"])
+  return render_template("about.html",
+                titleText=pageTitleDict["about"],
+                headerText=headerDict["about"])
+
+@app.route("/login")
+def login():
+  return render_template("access-area.html",
+                titleText=pageTitleDict["access"],
+                headerText=headerDict["access"])
+
+@app.route("/private")
+def private():
+  return render_template("private.html",
+                titleText=pageTitleDict["private"],
+                headerText=headerDict["private"])
 
 if __name__ == '__main__':
   app.run(debug=True)
--- meta_tags_dict.py
+++ meta_tags_dict.py
@@ -1,8 +1,17 @@
-
 pageTitleDict = {
   "index": "Life and weight balance",
   "historical": "Historical graphic - Life and weight balance",
   "about": "About this project - Life and weight balance",
-  "trips": "Trips to Spain - Life and weight balance" 
+  "trips": "Trips to Spain - Life and weight balance",
+  "access": "Access to personal area - Life and weight balance",
+  "private": "Private Area - Life and weight balance",
 }
 
+headerDict = {
+  "index": "Welcome to my index",
+  "historical": "Chart with historic data",
+  "about": "About this site",
+  "trips": "Trips",
+  "access": "Access to private area",
+  "private": "Private area",
+}
