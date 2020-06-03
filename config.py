f10bd16d550b475393456da035605c412f8c2c38
Paco Contreras <pacocontrerasdct@gmail.com>
2020-06-03 18:18:14 +0100

    added config file for flask-sqlalchemy

---
+++ config.py
@@ -0,0 +1,19 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+class Config:
+  """Set Flask configuration variables from .env file."""
+  
+  # General Flask config
+  SECRET_KEY = environ.get('SECRET_KEY')
+  FLASK_ENV = environ.get('FLASK_ENV')
+  FLASK_APP = 'app.py'
+  FLASK_DEBUG = 1
+
+  # Database
+  SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+  SQLALCHEMY_ECHO = True
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
